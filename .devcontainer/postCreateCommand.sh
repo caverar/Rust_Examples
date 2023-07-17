e5fbbda0cd9aa3b6a7ef82bd02e28d92e80f44d3
Camilo <camilovera9811@gmail.com>
2023-07-17 02:03:53 +0000

    Docker Image updated to generic one

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,6 @@
-sudo apt-get update
-sudo apt-get install yadm bat -y
+# sudo apt update               # Not needed, due to the previous execution from "devcontainer.json"
+# sudo apt install zsh -y       # Not needed, due to the previous execution from "devcontainer.json"
+sudo apt install yadm bat -y    # yadm is a dotfiles manager git clone and bat is a cat replacement
 
 # Install lsd (ls replacement)
 cd $HOME
@@ -7,7 +8,11 @@ wget "https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.
 sudo dpkg -i lsd_0.23.1_amd64.deb
 rm lsd_0.23.1_amd64.deb
 
-# Install zsh plugins
+# Install oh-my-zsh
+sudo rm -rf ~/.oh-my-zsh    # Remove previous installation
+zsh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
@@ -19,4 +24,4 @@ yadm clone -b devcontainers https://github.com/caverar/dotFiles.git
 
 # Enter the project folder
 cd /workspaces/Rust_Examples
-exec zsh
+# exec zsh
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,26 @@
-FROM mcr.microsoft.com/devcontainers/rust:1-1-bookworm
+# Official Microsoft Dev Container Rust Image
+# FROM mcr.microsoft.com/devcontainers/rust:1-1-bookworm
+
+# Official Rust Organization Image
+FROM rust:1.71.0-bookworm
+
+ARG USERNAME=rust_user
+ARG USER_UID=1000
+ARG USER_GID=$USER_UID
+
+# Create the user
+RUN groupadd --gid $USER_GID $USERNAME \
+    && useradd --uid $USER_UID --gid $USER_GID -m $USERNAME \
+    # [Optional] Add sudo support. Omit if you don't need to install software after connecting.
+    && apt update \
+    && apt install -y sudo \
+    && echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME \
+    && chmod 0440 /etc/sudoers.d/$USERNAME
+
 # Install the xz-utils package
-RUN apt-get update && apt-get install -y xz-utils
+RUN apt update && apt install -y xz-utils
+
+# # [Optional] Set the default user. Omit if you want to keep the default as root.
+USER $USERNAME
 
 
