5fb44cf416c175bee5b44a12c03a4d7437f053f2
Camilo <camilovera9811@gmail.com>
2023-09-01 01:25:58 -0500

    .

--- R0_TEST/src/main.rs
+++ R0_TEST/src/main.rs
@@ -3,14 +3,29 @@
 use std::io;
 fn main() {
     // Print to console with arguments
-    println!("Hello, world! {1}, {0}", 3, 2);
+    // println!("Hello, world! {1}, {0}", 3, 2);
 
-    // Print console with a named tuple argument
-    println!("{name:?}", name = (3, "hello"));
-    println!("{decimal:X}", decimal = 255);
+    // Print to console with a named tuple argument
+    // println!("{name:?}", name = (3, "hello"));
+    // Print to console with a named decimal argument in hex
+    // println!("{decimal:X}", decimal = 255);
 
-    // Read from console
+    // Mutable string
+    // let mut requested_operation: String = String::new();
+
+    // Read from console and store in requested_operation
+    // io::stdin()
+    //     .read_line(&mut requested_operation)
+    //     .expect("Failed to read line");
+    // println!("You requested: {}", requested_operation);
+
+    // CLI Menu
+    println!("Please select an operation: ");
     let mut requested_operation: String = String::new();
-    io::stdin().read_line(&mut requested_operation).unwrap();
-    print!("You requested: {}", requested_operation);
+    io::stdin()
+        .read_line(&mut requested_operation)
+        .expect("Failed to read line");
+    if requested_operation == "1" {
+        println!("Operation 1");
+    }
 }
